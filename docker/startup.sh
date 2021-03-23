28c18ad8e2fcf9e1755cf868d51d7b684fc6dce7
rc1021 <rc.701021@gmail.com>
2021-03-23 22:16:40 +0800

    startup.sh add crontab

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+(crontab -l && echo "0 12 * * * /usr/bin/certbot renew --quiet") | crontab -
+
+(crontab -l && echo "* * * * * cd /app && php artisan schedule:run >> /dev/null 2>&1") | crontab -
+
 sed -i "s,LISTEN_PORT,$PORT,g" /etc/nginx/nginx.conf
 
 /usr/bin/supervisord -c /app/docker/supervisord.conf
