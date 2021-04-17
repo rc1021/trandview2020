ac2d394cfaa5b3fd039e091678847c0688f2c85a
rc1021 <rc.701021@gmail.com>
2021-04-17 15:44:00 +0800

    startup crontab

--- docker/startup.sh
+++ docker/startup.sh
@@ -2,7 +2,9 @@
 
 (crontab -l && echo "0 12 * * * /usr/bin/certbot renew --quiet") | crontab -
 
-(crontab -l && echo "* * * * * cd /app && php artisan schedule:run >> /dev/null 2>&1") | crontab -
+(crontab -l && echo "* * * * * php /app/artisan schedule:run >> /dev/null 2>&1") | crontab -
+
+/usr/sbin/crond
 
 sed -i "s,LISTEN_PORT,$PORT,g" /etc/nginx/nginx.conf
 
